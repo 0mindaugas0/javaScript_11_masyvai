66251063a1f6f19e5a42e45b22394dd8293941cb
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-10-24 10:14:13 +0300

    masyvų užduotys

---
+++ script11-1.js
@@ -0,0 +1,29 @@
+const masyvas  = [3, 5, 6, 8];
+console.log(masyvas);
+
+console.log(masyvas.length);
+
+masyvas.push(3, 5, 6.325);
+console.log(masyvas);
+
+masyvas.unshift(10, 0 , 0, 10);
+console.log(masyvas);
+
+console.log(masyvas.length);
+
+masyvas.shift();
+console.log(masyvas);
+
+masyvas.pop();
+masyvas.pop();
+console.log(masyvas);
+
+const masyvas2 = ["vienas", "du", "trys"];
+console.log(masyvas2);
+
+masyvas2.push("keturi","penki", "šeši", "septyni");
+console.log(masyvas2);
+
+
+
+
